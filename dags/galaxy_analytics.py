139dca619ca6f938842f859815f28d4a6f3abf95
shri-astro <shrividya.hegde@astronomer.io>
2025-09-10 15:31:56 -0600

    Update galaxy_analytics.py

--- dags/galaxy_analytics.py
+++ dags/galaxy_analytics.py
@@ -11,4 +11,33 @@ _DUCKDB_TABLE_NAME = os.getenv("DUCKDB_TABLE_NAME", "galaxy_data")
 
 
 @asset()
-#Fill in the asset function here!
+def analyze_galaxies(
+   duckdb_instance_name: str = _DUCKDB_INSTANCE_NAME,
+   table_name: str = _DUCKDB_TABLE_NAME,
+) -> None:
+   """
+   Analyze the galaxy data by creating an analytics
+   table (in the logs!) with the count of galaxies
+   by type.
+
+   In production, you would write this data to a
+   persistent table somewhere and perhaps use it
+   in an analytics dashboard.
+   """
+   import duckdb
+   from tabulate import tabulate
+
+   cursor = duckdb.connect(duckdb_instance_name)
+
+   query = f"""
+      SELECT type_of_galaxy, COUNT(*) AS count
+      FROM {_DUCKDB_TABLE_NAME}
+      GROUP BY type_of_galaxy
+      ORDER BY count DESC
+   """
+
+   galaxy_analysis_df = cursor.sql(query).df()
+   t_log.info(tabulate(galaxy_analysis_df, headers="keys", tablefmt="pretty"))
+   cursor.close()
+
+   return galaxy_analysis_df
